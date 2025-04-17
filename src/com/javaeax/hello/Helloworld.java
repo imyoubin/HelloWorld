287c51197ab5a8cfff052445f31593bd99ff394f
imyoubin <imyoubin@hanmail.net>
2025-04-17 10:10:31 +0900

    2025.04.17 am10:10

---
+++ src/com/javaeax/hello/Helloworld.java
@@ -0,0 +1,19 @@
+/*
+작성자 임유빈
+작성일 2025 04 14
+설명 Helloworld 출력
+*/
+
+package com.javaeax.hello;
+
+
+public class Helloworld {
+
+	public static void main(String[] args){
+	
+		//문자열을 화면에 출력하는 함수
+		System.out.println("Hello World!");
+		//git 버전만들기
+		
+	}
+}
